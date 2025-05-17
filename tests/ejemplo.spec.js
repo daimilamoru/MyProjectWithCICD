543c497c88a8921b7337cc13ef0c38f4058a412f
daimi <daimilamoru2018@gmail.com>
2025-05-17 10:11:56 -0300

    Cambio en título del test para probar CI

--- tests/ejemplo.spec.js
+++ tests/ejemplo.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Página de ejemplo tiene título', async ({ page }) => {
+test('Probando CI/CD', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
 });
