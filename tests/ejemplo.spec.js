f5af20795d8698426130e0d79b1a351dddd0a5c9
daimi <daimilamoru2018@gmail.com>
2025-05-17 11:36:12 -0300

    Add click action

--- tests/ejemplo.spec.js
+++ tests/ejemplo.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Probando CI/CD', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+  await page.getByRole('link', { name: 'Get started' }).click();
   const edad=0;
-  expect(edad).toBe(1);
+  expect(edad).toBe(0);
 });
