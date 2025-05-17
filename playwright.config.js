a9c8681942a621ac301e1066275686f0e105bd86
daimi <daimilamoru2018@gmail.com>
2025-05-17 10:29:01 -0300

    Fix: set headless mode to true for GitHub CI

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 5000,
     baseURL: 'https://playwright.dev/', // Cámbialo si usás otra URL base
