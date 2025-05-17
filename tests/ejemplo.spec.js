885e639e0760ea2b4f4207fffa192312d1cec2df
daimi <daimilamoru2018@gmail.com>
2025-05-17 11:25:01 -0300

    add field age

--- tests/ejemplo.spec.js
+++ tests/ejemplo.spec.js
@@ -3,4 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Probando CI/CD', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
+  const edad=0;
+  expect(edad).toBe(0);
 });
